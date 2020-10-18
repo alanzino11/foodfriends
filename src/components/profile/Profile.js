f62ea631a53cf0815ae9adb7bbf5740e967e67a4
alanzino11 <annalanzino@ufl.edu>
2020-10-18 11:35:03 -0400

    added editing

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,5 +1,21 @@
 import React, {useState} from 'react';
-import { IconButton, Button } from "@chakra-ui/core";
+import { 
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuOptionGroup,
+    MenuItemOption,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+} from "@chakra-ui/core";
 
 import './Profile.css';
 import image from '../../images/image.png';
@@ -9,6 +25,9 @@ const Profile = ({profile, setDropdownIndex}) => {
     const [isFollowing, setFollowing] = useState("Follow");
     const [icon, setIcon] = useState("triangle-down");
     const [showSimilarProfiles, setShowSimilarProfiles] = useState(false);
+    const [isEditing, setEditing] = useState(false);
+    const [diet, setDiet] = useState(profile.diet);
+    const [popoverOpen, setPopoverOpen] = useState(false)
 
     const dropdown = () => {
         icon === "triangle-down" ? setIcon("triangle-up") : setIcon("triangle-down");
@@ -16,11 +35,21 @@ const Profile = ({profile, setDropdownIndex}) => {
         showSimilarProfiles ? setDropdownIndex(-1) : setDropdownIndex(profile.index);
     }
 
+    const editDiet = (newDiet) => {
+        setDiet(newDiet)
+        setPopoverOpen(!popoverOpen)
+    }
+
     return (
         <div className="profile">
             <div className="content">
                 <div className="edit">
-                    <Button>Edit</Button>
+                    <Button 
+                        onClick={() => setEditing(!isEditing)}
+                        variantColor={isEditing ? "red": "gray"}
+                    >
+                        {isEditing ? "Save Edit" : "Edit"}
+                    </Button>
                 </div>
                 <div className="image">
                     <img src={image} alt=""/>
@@ -29,7 +58,39 @@ const Profile = ({profile, setDropdownIndex}) => {
                     <span className="name"><b>{profile.name}</b></span>
                 </div>
                 <div className="dietpills">
-                    <div className="pill"><Pill data={profile.diet}/></div>
+                    <div className="pill"><Pill data={diet}/></div>
+                    {
+                        isEditing ?
+                        ( <Popover usePortal isOpen={popoverOpen}>
+                            <PopoverTrigger>
+                                <IconButton size="sm" icon="edit" onClick={() => setPopoverOpen(true)}/>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <PopoverArrow />
+                                <PopoverHeader>Choose a new diet.</PopoverHeader>
+                                <PopoverCloseButton />
+                                <PopoverBody>
+                                    <Button variantColor="white" onClick={() => editDiet("vegan")}><Pill data="vegan"/></Button>
+                                    <Button variantColor="white" onClick={() => editDiet("keto")}><Pill data="keto"/></Button>
+                                    <Button variantColor="white" onClick={() => editDiet("gluten-free")}><Pill data="gluten-free"/></Button>
+                                    <Button variantColor="white" onClick={() => editDiet("vegetarian")}><Pill data="vegetarian"/></Button>
+                                </PopoverBody>
+                            </PopoverContent>
+                        </Popover> ) : null
+                    }
+
+                    {/* <Menu>
+                        <MenuButton as={Button} rightIcon="chevron-down" border="1px solid grey">
+                        Diet
+                        </MenuButton>
+                        <MenuList minWidth="240px">
+                        <MenuOptionGroup title="Diet" type="checkbox">
+                            <MenuItemOption value="vegan">Vegan</MenuItemOption>
+                            <MenuItemOption value="keto">Keto</MenuItemOption>
+                            <MenuItemOption value="gluten-free">Gluten-Free</MenuItemOption>
+                        </MenuOptionGroup>
+                        </MenuList>
+                    </Menu> */}
                 </div>
                 <div className="info">
                     <b>{profile.city}, FL</b> &nbsp; | &nbsp; <b>{profile.priceRange}</b>
